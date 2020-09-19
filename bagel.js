0b0e30ef7a708faeb45f6b1a06f404474a187f8a
pecancake <33271004+pecancake@users.noreply.github.com>
2020-09-19 16:19:13 -0400

    Objects and Ticking
    - Added object support
    - Added ticker to Bagel, that computes object step/draw codes
    - Created spawn function in example, that lets user spawn any number of randomly coloured bouncing squares

--- bagel.js
+++ bagel.js
@@ -1,13 +1,32 @@
+class BagelObject {
+    constructor(bagel, init, step, draw) {
+        this.bagel = bagel;
+        this.init = init;
+        this.step = step;
+        this.draw = draw;
+    }
+}
+
 class Bagel {
     constructor(canvasid) {
         // basic attributes
         this.canvas = document.getElementById(canvasid);
         this.ctx = this.canvas.getContext('2d');
         this.canvasData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
-        this.blank = this.ctx.createImageData(this.canvas.width, this.canvas.height);
 
         // drawing placeholders
         this.drawCol = [0, 0, 0, 255];  // RGBA
+
+        // object data & ticker
+        this.bagelObjects = [];
+        this.tickRate = 60;
+        this.tickStarted = false;
+        
+        // DEBUG FPS STUFF
+        this.FPSLastTime = Date.now();
+        this.FPSCounter = 0;
+        // TODO - set draw target function so you can pass multiple canvas ids to
+        //        bagel and have it switch between draw targets
     }
 
     drawSetCol(col) {
@@ -29,7 +48,7 @@ class Bagel {
     }
 
     clear() {
-        this.canvasData = this.blank;
+        this.canvasData = this.ctx.createImageData(this.canvas.width, this.canvas.height);
     }
 
     update() {
@@ -43,4 +62,45 @@ class Bagel {
             }
         }
     }
+
+    createObject(init, step, draw) {
+        var obj = new BagelObject(this, init, step, draw);
+        obj.init();
+
+        this.bagelObjects.push(obj);
+    }
+
+    tick = () => {
+        for (var obj of this.bagelObjects) {
+            obj.step();
+        }
+
+        this.clear();
+        for (var obj of this.bagelObjects) {
+            obj.draw();
+        }
+        this.update();
+
+        if (this.FPSLastTime < Date.now() - 1000) {
+            console.log(`FPS: ${this.FPSCounter}`);
+            this.FPSLastTime = Date.now();
+            this.FPSCounter = 0;
+        }
+        else
+        {
+            this.FPSCounter += 1;
+        }
+    };
+
+    tickStart() {
+        this.ticker = setInterval(this.tick, 1000/this.tickRate);
+    }
+
+    tickStop() { 
+        clearInterval(this.ticker);
+    }
+}
+
+function randInt(a, b) {
+    Math.floor(Math.random() * b) + a; 
 }
--- example/script.js
+++ example/script.js
@@ -1,7 +1,36 @@
 var bagel = new Bagel("drawCanvas");
 
-//bagel.clear();
-bagel.drawSetCol([255, 0, 0, 255]);
-bagel.drawPixel(20, 20);
-bagel.drawRect(60, 60, 80, 80);
-bagel.update();
+function spawn(val) {
+    for (var i=0; i!=val; i++) {
+        bagel.createObject(
+            function() {
+                this.x = Math.floor(Math.random() * 350) + 50;//20;//randInt(50, 350);
+                this.y = Math.floor(Math.random() * 350) + 50;//20;//randInt(50, 350);
+                this.hspeed = Math.floor(Math.random() * 6) + 2;
+                this.vspeed = Math.floor(Math.random() * 6) + 2;
+                this.col = [Math.floor(Math.random() * 255) + 0,
+                    Math.floor(Math.random() * 255) + 0,
+                    Math.floor(Math.random() * 255) + 0, 255];
+            },
+            function() {
+                
+                if (this.x >= 397 || this.x <= 2) {
+                    this.hspeed *= -1;
+                }
+                if (this.y >= 397 || this.y <= 2) {
+                    this.vspeed *= -1;
+                }
+
+                this.x += this.hspeed;
+                this.y += this.vspeed;
+            },
+            function() {
+                this.bagel.drawSetCol(this.col)
+                this.bagel.drawRect(this.x-3, this.y-3, this.x+3, this.y+3);
+                //this.bagel.drawRect(this.x-5, this.y-5, this.x+5, this.y+5);
+            }
+        );
+    }
+}
+
+bagel.tickStart();
